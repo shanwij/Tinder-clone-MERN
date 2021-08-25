e9171cd65e33185d7be4b67910bb838ea8f4f45d
IT18132588 <IT18132588@my.sliit.lk>
2021-08-26 01:47:57 +0530

    Update files

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className='header'>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            <img 
+                className="header__large"
+                src="https://img.icons8.com/fluency/48/000000/--tinder.png"
+                alt=""
+            />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+        </div>
+    );
+}
+
+export default Header;
